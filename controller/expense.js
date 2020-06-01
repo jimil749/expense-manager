e0aced8759523e70aae5342da4ce78af81035001
Jimil Desai <jimildesai42@gmail.com>
2020-06-02 01:26:30 +0530

    preview query

--- controller/expense.js
+++ controller/expense.js
@@ -5,7 +5,6 @@
     3) View User's expense : done
     4) Testing : done
     Frontend : 
-    1) User login and sign-up
     2) Add
     3) Expense Overview
 
@@ -39,6 +38,43 @@ expenseRouter.get('/', async (request, response) => {
     const user = await User.findById(decodedToken.id)
     const expenses = await Expense.find({user: user._id}) 
     response.json(expenses.map(expense => expense.toJSON()))  
+})
+
+expenseRouter.get('/preview', async(request, response) => {
+    const token = getToken(request)
+    if (token == null) {
+        return response.status(401).json({ error: 'token missing or invalid' })
+    }
+
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+    
+    if (!token || !decodedToken) {
+        return response.status(401).json({ error: 'token missing or invalid' })
+    }  
+
+    const date = new Date()
+    const y = date.getFullYear()
+    const m = date.getMonth()
+    const firstDay = new Date(y, m, 1)
+    const lastDay = new Date(y, m+1, 0)
+
+    const today = new Date()
+    today.setUTCHours(0,0,0,0)
+
+    const tomorrow = new Date()
+    tomorrow.setUTCHours(0,0,0,0)
+    tomorrow.setDate(tomorrow.getDate()+1)
+
+    const yesterday = new Date()
+    yesterday.setUTCHours(0,0,0,0)
+    yesterday.setDate(yesterday.getDate-1)
+
+    const expense = await Expense.find({
+        date: {$gte: firstDay, $lte: lastDay}
+    })
+
+    response.json(expense.map(expenses => expenses.toJSON()))
+
 })
 
 expenseRouter.post('/', async(request, response) => {    
